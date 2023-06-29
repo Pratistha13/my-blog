349adbf66dd1504ae35a16b9135e8614f7c53178
Pratistha Mishra <56651666+Pratistha13@users.noreply.github.com>
2023-06-30 00:01:52 +0300

    made some changes in server js to connect front end and backend

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -28,7 +28,7 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
 
   
   if (article) {
-    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    res.json(article);
   } else {
     res.send("That article doesnot exist");
   }
@@ -46,7 +46,7 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   const article = await db.collection('articles').findOne({name});
 
   if (article) {
-    res.send(article.comments);
+    res.json(article);
   } else {
     res.send("That article doesnot exist!!");
   }
